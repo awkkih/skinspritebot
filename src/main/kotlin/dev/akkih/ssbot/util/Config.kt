fe0e3734ac0316b14832baa70dd8f47f2ff1ef4b
awkkih <joao.alduan@gmail.com>
2026-07-17 18:17:53 -0300

    build: fix .env

--- src/main/kotlin/dev/akkih/ssbot/util/Config.kt
+++ src/main/kotlin/dev/akkih/ssbot/util/Config.kt
@@ -3,9 +3,11 @@ package dev.akkih.ssbot.util
 import io.github.cdimascio.dotenv.dotenv
 
 class Config {
-    private val dotenv = dotenv()
+    private val dotenv = dotenv {
+        ignoreIfMissing = true
+    }
 
-    val DISCORD_TOKEN = dotenv["DISCORD_TOKEN"]
-    val WORKER_BASE_URL = dotenv["WORKER_BASE_URL"]
-    val WORKER_UPLOAD_SECRET = dotenv["WORKER_UPLOAD_SECRET"]
+    val DISCORD_TOKEN = dotenv["DISCORD_TOKEN"] ?: System.getenv("DISCORD_TOKEN")
+    val WORKER_BASE_URL = dotenv["WORKER_BASE_URL"] ?: System.getenv("WORKER_BASE_URL")
+    val WORKER_UPLOAD_SECRET = dotenv["WORKER_UPLOAD_SECRET"] ?: System.getenv("WORKER_UPLOAD_SECRET")
 }
